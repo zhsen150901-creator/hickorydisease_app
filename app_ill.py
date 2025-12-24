76edacf71f53f68fb1887337c6b85d281fdd3277
zhsen150901-creator <zhsen150901@gmail.com>
2025-12-24 14:36:36 +0800

    Update app_ill.py

--- app_ill.py
+++ app_ill.py
@@ -9,7 +9,7 @@ st.set_page_config(
 )
 
 # ========== 加载模型 ==========
-model = load(r"C:\Users\张森\PycharmProjects\PythonProject\disease_model_poly.pkl")
+model = load(r"disease_model_poly.pkl")
 coef = model["coef"]
 scaler = model["scaler"]
 feature_names = model["feature_names"]
@@ -154,3 +154,4 @@ if st.button("开始预测"):
 
 else:
     st.warning("请填写以上参数后，点击“开始预测”进行风险评估。")
+
