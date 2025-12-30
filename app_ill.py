ddd51d9e5583ef04bdaaebc762d699bb9d5a68a6
zhsen150901-creator <zhsen150901@gmail.com>
2025-12-30 21:44:42 +0800

    app_ill.py

--- app_ill.py
+++ app_ill.py
@@ -4,12 +4,12 @@ from joblib import load
 
 # ========== 基本设置 ==========
 st.set_page_config(
-    page_title="山核桃黑籽病发病率预测系统",
+    page_title="山核桃黑籽病发病率预警系统",
     layout="centered",
 )
 
 # ========== 加载模型 ==========
-model = load(r"C:\Users\张森\PycharmProjects\PythonProject\disease_model_poly.pkl")
+model = load(r"disease_model_poly.pkl")
 coef = model["coef"]
 scaler = model["scaler"]
 feature_names = model["feature_names"]
@@ -18,7 +18,7 @@ SPORE_FACTOR = float(model.get("spore_factor", 395.0))  # 默认 395
 # ========== 页面标题 ==========
 st.markdown(
     """
-    <h2 style="text-align:center; margin-bottom:0.2rem;">山核桃黑籽病发病率预测系统</h2>
+    <h2 style="text-align:center; margin-bottom:0.2rem;">山核桃黑籽病发病率预警系统</h2>
     <p style="text-align:center; color: #555;">
         输入 5 月 15 日至 8 月 15 日高温时长、5/7 月三种孢子周峰值及经营水平，系统将评估黑籽病风险等级
     </p>
@@ -154,3 +154,4 @@ if st.button("开始预测"):
 
 else:
     st.warning("请填写以上参数后，点击“开始预测”进行风险评估。")
+
